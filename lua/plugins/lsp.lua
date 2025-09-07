792240a26bb36b1b5622e01ea385edeee1a644ef
LiuranShen <LiuranShen@noreply.gitcode.com>
2025-09-07 19:06:26 +0800

    feat(lsp): 启用 LazyVim 默认的 LSP 配置
    - 启用 nvim-lspconfig、mason.nvim、mason-lspconfig.nvim 和 nvim-cmp 插件
    - 此修改将允许使用 LazyVim 的默认 LSP 功能，而不是禁用它们

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
   -- 禁用 LazyVim 默认的 LSP 配置
-  { "neovim/nvim-lspconfig", enabled = false },
-  { "williamboman/mason.nvim", enabled = false },
-  { "williamboman/mason-lspconfig.nvim", enabled = false },
-  { "hrsh7th/nvim-cmp", enabled = false }, -- 禁用内置补全，使用 coc 的补全
+  { "neovim/nvim-lspconfig", enabled = true },
+  { "williamboman/mason.nvim", enabled = true },
+  { "williamboman/mason-lspconfig.nvim", enabled = true },
+  { "hrsh7th/nvim-cmp", enabled = true }, -- 禁用内置补全，使用 coc 的补全
 }
