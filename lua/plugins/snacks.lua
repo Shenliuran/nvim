964270a2544e3e31db04612d6570b933d313cfcd
LiuranShen <LiuranShen@noreply.gitcode.com>
2025-09-07 19:44:18 +0800

    feat(lua): 添加 snacks 插件配置
    - 引入 folke/snacks.nvim 插件
    - 配置 animate 和 dashboard 选项
    - 自定义 dashboard 样式和快捷键

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,34 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+	animate = { enabled = true },
+    dashboard = {
+      preset = {
+        pick = function(cmd, opts)
+          return LazyVim.pick(cmd, opts)()
+        end,
+        header = [[
+        ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
+        ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
+        ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
+        ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z         
+        ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║           
+        ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝           
+ ]],
+        -- stylua: ignore
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = "🔎 ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = "➕ ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = "🔍 ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = "🕙 ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = "⚙️ ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = "◀️ ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = "💤 ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "⚡ ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = "⭕ ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+    },
+  },
+}
