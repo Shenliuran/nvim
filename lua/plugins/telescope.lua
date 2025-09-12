eed8802772c05ad92abbdda3d874889d7afb4327
Liuran Shen <shenliuran1999@foxmail.com>
2025-09-12 14:04:41 +0800

    feat(lua): 添加 telescope 插件配置并忽略 .git 目录
    - 在 lua/plugins 目录下新增 telescope.lua 文件
    - 配置 telescope 插件，设置文件忽略模式
    - 忽略 .git 目录及其中的所有文件
    - 可选配置：忽略 node_modules 目录和日志文件

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("telescope").setup({
+      defaults = {
+        -- 全局忽略模式：忽略 .git 目录及其中的所有文件
+        file_ignore_patterns = {
+          ".git/", -- 关键配置：忽略 .git 目录
+          "node_modules/", -- 可选：忽略其他不需要的目录
+        },
+      },
+      pickers = {
+        live_grep = {
+          additional_args = function()
+            return {
+              "--glob", "!.git/*",  -- 忽略 .git 目录下的所有内容
+              "--glob", "!*.log",   -- 可选，忽略日志文件
+            }
+          end
+        }
+      }
+    })
+  end,
+}
