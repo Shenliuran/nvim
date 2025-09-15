2f9aec4f1d1065326e6b4a8249d781434aa8f6ef
Liuran Shen <shenliuran1999@foxmail.com>
2025-09-15 08:17:35 +0800

    setup(ufo): 优化代码折叠配置
    - 设置 foldcolumn、foldlevel 等选项，以实现更好的代码折叠体验
    - 添加 provider_selector 函数，优先使用 treesitter 进行代码折叠
    - 移除错误的快捷键映射，保留正确的折叠和展开操作

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -5,16 +5,23 @@ if not vim.g.vscode then
     config = function()
       local ufo = require('ufo')
       
+      vim.opt.foldcolumn = '1'
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+      vim.opt.foldenable = true
+
       ufo.setup({
         open_fold_hl_timeout = 0,
         fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
           return virtText
+        end,
+
+        provider_selector = function(bufnr, filetype, buftype)
+          return { 'treesitter', 'indent' }
         end
       })
       
       -- 正确的快捷键映射（使用ufo提供的正确函数）
-      vim.keymap.set('n', 'zc', ufo.closeFoldsWith, { desc = '折叠当前块' })
-      vim.keymap.set('n', 'zo', ufo.openFoldsExceptKinds, { desc = '展开当前块' })
       vim.keymap.set('n', 'zM', ufo.closeAllFolds, { desc = '折叠所有' })
       vim.keymap.set('n', 'zR', ufo.openAllFolds, { desc = '展开所有' })
     end
