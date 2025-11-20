fc7b74883c13ad14c1f7e4868c613c5349c52c59
Liuran Shen <shenliuran1999@foxmail.com>
2025-11-20 14:19:47 +0800

    feat(spectre): 增强 Spectre 插件快捷键与界面配置
    新增多个便捷操作快捷键，包括当前文件搜索替换、可视模式选中内容搜索替换等功能。
    同时优化界面显示效果，添加自定义分隔线提升视觉体验。

--- lua/plugins/spectre.lua
+++ lua/plugins/spectre.lua
@@ -2,12 +2,17 @@ return {
   "nvim-pack/nvim-spectre",
   dependencies = "nvim-lua/plenary.nvim",
   keys = {
-    { "<leader>sr", "<cmd>Spectre<CR>", desc = "全局正则替换" },
+    { "<leader>ss", "<cmd>Spectre<CR>", desc = "Spectre: 全局正则替换" },
+    { "<leader>sw", "<cmd>Spectre %<CR>", desc = "Spectre: 当前文件搜索替换" },
+    { "<leader>sv", mode = "v","<esc><cmd>lua require('spectre').open_visual()<CR>", desc = "Spectre: 可是换选中搜索替换" },
+    { "<leader>sf", mode = "v", "<cmd>lua require('spectre').open_file_search()<CR>", desc = "Spectre: 仅当前文件（进阶）"},
   },
   config = function()
     require("spectre").setup({
       open_cmd = "new", -- 打开方式：new 窗口/vsplit 分屏等
       live_update = true, -- 实时更新预览
+      line_sep_start = "┌─────────────────────────────────────────────────────────────────────────┐",
+      line_sep_end = "  └─────────────────────────────────────────────────────────────────────────┘",
       highlight = {
         ui = "String", -- 界面高亮组
         search = "DiffChange", -- 搜索匹配高亮
