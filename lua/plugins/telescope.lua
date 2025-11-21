8f12432ed64cec0e48561fc36aa1af4ad7e37105
Liuran Shen <shenliuran1999@foxmail.com>
2025-11-21 10:16:25 +0800

    feat(telescope): 增强文件与文本搜索功能
    新增在指定目录中搜索文本的功能，并更新现有搜索键绑定描述以提高准确性。
    通过 `<leader>fG` 可触发交互式目录输入搜索，提升局部代码检索效率。

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -23,7 +23,25 @@ return {
     })
 
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "实时文本搜索" })
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "查找文件" })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "全局实时文本搜索" })
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "全局查找文件" })
+
+    local function live_grep_in_directory()
+      -- 弹出一个输入框让用户输入目录路径
+      vim.ui.input({prompt = "搜索目录: "}, function(dir)
+        -- 检查用户是否输入内容
+        if dir and dir ~= "" then
+          -- 调用 live_grep，并将 search_dirs 设置为用户输入的目录
+          -- 这样会自动继承上面 setup 中为 live_grep 配置的 additional_args
+          builtin.live_grep({
+            search_dirs = { dir },
+          })
+        end
+      end)
+    end
+
+    vim.keymap.set('n', '<leader>fG', live_grep_in_directory, {
+      desc = "在指定目录中搜索文本"
+    })
   end,
 }
