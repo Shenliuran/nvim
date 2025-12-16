bd643b8ccee21f4cc5b678c1ef55c827a18c44e3
Liuran Shen <3224575233@qq.com>
2025-12-16 15:07:18 +0800

    feat(noice) 修改Noice cmdline的位置
    将Noice cmdline的位置从屏幕中间调整至屏幕上方，以免遮挡视线

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -3,6 +3,13 @@ return {
   event = "VeryLazy",
   opts = {
     -- add any options here
+    presets = {
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+      inc_rename = false,
+      lsp_doc_border = false,
+    }
   },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
@@ -11,5 +18,6 @@ return {
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
-    }
+  }
+  
 }
