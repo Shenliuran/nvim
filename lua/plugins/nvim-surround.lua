b8be3b6280f104fcca78a87bd5505fd2cb6b915f
Liuran Shen <shenliuran1999@foxmail.com>
2025-12-24 15:28:51 +0800

    refactor(nvim-surround): 删除重复的配置
    删除VeryLazy配置，保留BufReadPost和BufNewFile

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -2,7 +2,6 @@ return {
   "kylechui/nvim-surround",
   event = { "BufReadPost", "BufNewFile" },
   version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
-  event = "VeryLazy",
   config = function()
     require( "nvim-surround" ).setup({
         -- Configuration here, or leave empty to use defaults
