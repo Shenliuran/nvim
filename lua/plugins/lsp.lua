a19349e0045f8ebca183ca23fed285285a8c2a3b
Liuran Shen <shenliuran1999@foxmail.com>
2026-01-16 11:11:08 +0800

    style(lsp): 修复错误缩进

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,38 +3,38 @@ return {
     "mason-org/mason.nvim",
     enabled = true,
     cmd = "Mason",
-      keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-      build = ":MasonUpdate",
-      opts_extend = { "ensure_installed" },
-      opts = {
-        ensure_installed = {
-          "stylua",
-          "shfmt",
-        },
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+    build = ":MasonUpdate",
+    opts_extend = { "ensure_installed" },
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shfmt",
       },
-      ---@param opts MasonSettings | {ensure_installed: string[]}
-      config = function(_, opts)
-        require("mason").setup(opts)
-        local mr = require("mason-registry")
-        mr:on("package:install:success", function()
-          vim.defer_fn(function()
-            -- trigger FileType event to possibly load this newly installed LSP server
-            require("lazy.core.handler.event").trigger({
-              event = "FileType",
-              buf = vim.api.nvim_get_current_buf(),
-            })
-          end, 100)
-        end)
+    },
+    ---@param opts MasonSettings | {ensure_installed: string[]}
+    config = function(_, opts)
+      require("mason").setup(opts)
+      local mr = require("mason-registry")
+      mr:on("package:install:success", function()
+        vim.defer_fn(function()
+          -- trigger FileType event to possibly load this newly installed LSP server
+          require("lazy.core.handler.event").trigger({
+            event = "FileType",
+            buf = vim.api.nvim_get_current_buf(),
+          })
+        end, 100)
+      end)
     
-        mr.refresh(function()
-          for _, tool in ipairs(opts.ensure_installed) do
-            local p = mr.get_package(tool)
-            if not p:is_installed() then
-              p:install()
-            end
+      mr.refresh(function()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local p = mr.get_package(tool)
+          if not p:is_installed() then
+            p:install()
           end
-        end)
-      end,
+        end
+      end)
+    end,
   },
   {
     "mason-org/mason-lspconfig.nvim",
@@ -52,7 +52,8 @@ return {
     opts = {
       servers = {
         lua_ls = {},
-        pyright = {},
+        ty = {},
+        ts_ls = {},
       },
     },
     config = function(_, opts)
