62befd1e713072bb7d99e6174537556c14ab4d8a
LiuranShen <LiuranShen@noreply.gitcode.com>
2026-01-16 20:25:23 +0800

    refactor(nvim-spectre): 搜索结果显示界面
    + 优化了spectre的搜索结果展示界面
    + 解决了输入`<leader>sw`无法使用的问题

--- lua/plugins/nvim-spectre.lua
+++ lua/plugins/nvim-spectre.lua
@@ -3,7 +3,7 @@ return {
   dependencies = "nvim-lua/plenary.nvim",
   keys = {
     { "<leader>ss", "<cmd>Spectre<CR>", desc = "Spectre: 全局正则替换" },
-    { "<leader>sw", "<cmd>Spectre %<CR>", desc = "Spectre: 当前文件搜索替换" },
+    { "<leader>sw", "<cmd>lua require('spectre').open_file_search()<CR>", desc = "Spectre: 当前文件搜索替换" },
     { "<leader>sv", mode = "v","<esc><cmd>lua require('spectre').open_visual()<CR>", desc = "Spectre: 可是换选中搜索替换" },
     { "<leader>sf", mode = "v", "<cmd>lua require('spectre').open_file_search()<CR>", desc = "Spectre: 仅当前文件（进阶）"},
   },
@@ -11,8 +11,9 @@ return {
     require("spectre").setup({
       open_cmd = "new", -- 打开方式：new 窗口/vsplit 分屏等
       live_update = true, -- 实时更新预览
-      line_sep_start = "┌─────────────────────────────────────────────────────────────────────────┐",
-      line_sep_end = "  └─────────────────────────────────────────────────────────────────────────┘",
+      line_sep_start = '┌─────────────────────────────────────────',
+      result_padding = '󰈿 ',
+      line_sep       = '└─────────────────────────────────────────',
       highlight = {
         ui = "String", -- 界面高亮组
         search = "DiffChange", -- 搜索匹配高亮
